817da519fb0a64c53379c17c240f7b517f1d8a53
rnutall <rnutall1@gmail.com>
2023-04-10 12:13:15 -0500

    updated file

--- index.js
+++ index.js
@@ -1,68 +1,79 @@
-function genLogo(answers);
-let inquirer = require('inquirer');
-let fetch = require("node-fetch");
+const inquirer = require("inquirer");
+const fs = require('fs');
+const {Circle, Square, Triangle}= require("./lib/shapes.js");
+const SVG = require("./lib/svg.js");
+let newssvg;
+let svgData;
+let sloagan;
+let shape;
+let shape_colornpm
 
-//prompt for text with 3 chars.
-const questions = [{
-message: "what characters would you like for your logo?"
- chars: ("Min 3 Characters."),
- input: (""),
+//prompt for slogan 
+const questions = [
+    {
+    name: "slogan",
+message: "what is your business slogan?",
  type: "input",
 
- //prompt for text color, then i can enter coloer keyword or [hexadecimal]
-    message: "What color would you like your text?"
- color:  (""),
-    input: (""),
+ //prompt for text color, then i can enter color keyword or [hexadecimal]
+name: "text_color",
+meassage: "what color is your logo text?",
     type: "input",
 
-    // promt for shape, presented with a list circle, triangle, square
-     message: "choose a shape Circle, Triangle, Square"
-     shapes: ("Circle, Triangle, Square"),
-     input: (""),
-    type: "input",
+ // promt for shape, presented with a list circle, triangle, square
+     name: "shape",
+     meassage: "Choose a shape Circle, Triangle, Square",
+     type: "input",
+     selection: ["Circle, Triangle, Square"],
 
     //promt for shape color, thern enter color keyword or [hexadecimal]
-    message: "What color would you like your shape?"
+    name: "shape_color",
+    message: "What color would you like your shape?",
+    type: "input"
 }];
 
-
-
-
-
-function writeToFile(ReadMe_server, data,) {}
-function writeToFile(ReadMe_server, data,) {
- fs.writeFile('data.csv', 'utf8', (error, data) => {
+// function to write to file and prompts.
+function makeFile(data) {
+ fs.writeFile("./deliverable/logo.svg", data, err => {
 
     if (err) {
-        console.log("File writing error", err);
-        }
-        else {
-            console.log("Successful file");
-        }
-        console.log(genlogo);
-
+        console.log("err");
+    }
     });
 }  
-
-//command line app that acceps usser input.
-
-
-
-
 //when all prompts are entered, then SVG IS CREATED 'logoSVG'
-//when the logo.SVG is opened in browser, then im shown a 300x200 px image that matches criteria entered      
+inquirer
+.prompt(questions)
+.then(answers => {
+    console.log(answers);
 
-// test 
+    if (answers.slogan.length > 0 && answers.slogan,length < 4){
+        slogan = answers.slogan;
+    }
+    else{
+        console.log("Enter three characters");
+        return;
+    }
+    text_color = answers.text_color;
+    switch(answers.shape){
+        case "Square":
+            shape = new Square;
+            break;
 
-//const shape = new Triangle();
-//shape.setColor("blue");
-//expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
-function init() {
-    inquirer
-    .prompt(questions)
-    .then((answers) =>{})
-}
+            case "Triangle":
+                shape = new "Triangle";
+                break;
+                
+                case  "Circle":
+                    shape = new Circle;
+                    break;
+    }
+    shape.setColor(answers.color);
+    newsvg = new SVG();
+    newsvg.setText(slogan, text_color);
+    newsvg.setShape(shape);
+    svgData = newsvg.render();
+    //console.log(svgData);
+    makeFile(svgData);
+    });
 
-       // console.log(answers)
-
-       init();
